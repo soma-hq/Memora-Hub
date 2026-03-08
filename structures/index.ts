b581db08ce9dea36ac5ee1bdcc18f313d3df6210
Jérémy <aime.jeremys@gmail.com>
2026-03-08 02:53:36 +0100

    🏗️ Updated remaining structures

--- structures/index.ts
+++
@@ -1,8 +0,0 @@
-export type { PageConfig, PageSection } from "./PageConfig";
-export { definePageConfig } from "./PageConfig";
-
-export type { ScriptConfig, ScriptCategory } from "./ScriptConfig";
-export { defineScriptConfig } from "./ScriptConfig";
-
-export type { PageConventions } from "./conventions";
-export { CONVENTIONS } from "./conventions";
